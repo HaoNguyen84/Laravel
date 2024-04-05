b7bc278580c79e86aecbf4288ff581bd9b133991
khoa <tkpkpin002@gmail.com>
2024-04-05 21:57:32 +0700

    user-update

---
+++ example-app/resources/views/crud_user/update.blade.php
@@ -0,0 +1,86 @@
+@extends('dashboard')
+
+@section('content')
+<main class="signup-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+
+
+            <!-- <form action="{{ route('user.postUpdateUser') }}" method="POST">
+                @csrf
+                <input name="id" type="hidden" value="{{$user->id}}">
+                <div class="form-group mb-3">
+                    <input type="text" placeholder="Name" id="name" class="form-control" name="name" value="{{ $user->name }}" required autofocus>
+                    @if ($errors->has('name'))
+                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                    @endif
+                </div>
+                <div class="form-group mb-3">
+                    <input type="text" placeholder="Email" id="email_address" class="form-control" value="{{ $user->email }}" name="email" required autofocus>
+                    @if ($errors->has('email'))
+                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                    @endif
+                </div>
+                <div class="form-group mb-3">
+                    <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
+                    @if ($errors->has('password'))
+                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                    @endif
+                </div>
+
+                <div class="d-grid mx-auto">
+                    <button type="submit" class="btn btn-dark btn-block">Update</button>
+                </div>
+            </form> -->
+
+
+            <div class="card text-center" style="padding:15px;">
+                <h4 class="text-primary">UPDATE</h4>
+            </div><br><br>
+
+            <div class="container">
+
+                <div class="container">
+                    <form action="{{ route('user.postUpdateUser') }}" method="POST">
+                        @csrf
+                        <input name="id" type="hidden" value="{{$user->id}}">
+                        <div class="form-group">
+                            <label for="Username">Username</label>
+                            <input name="name" type="text" class="form-control" placeholder="nhap ma sinh vien" value="{{ $user->name }}" id="name" required autofocus>
+                            @if ($errors->has('name'))
+                            <span class="text-danger">{{ $errors->first('name') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="email">email</label>
+                            <input type="text" class="form-control" placeholder="nhap email" value="{{ $user->email }}" id="email_address">
+                            @if ($errors->has('email'))
+                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="password">nhập password</label>
+                            <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
+                            @if ($errors->has('password'))
+                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                        
+                        <input type="submit" name="submit" class="btn btn-primary" style="float:left;" value="Edit">
+                    </form>
+
+                </div>
+                <br><br><br><br>
+            </div>
+            <footer class="py-5 bg-dark">
+                <div class="container">
+                    <p class="m-0 text-center text-white">Lập trình web @ 01/2024</p>
+                </div>
+            </footer>
+
+
+
+        </div>
+    </div>
+</main>
+@endsection
